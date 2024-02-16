077e787c281fcd78b8865161bf4ceec7d7c93877
Alexander Lichtenstein <alexander@lichtenstein.com>
2024-02-16 14:11:22 +0100

    corrections

--- app.py
+++ app.py
@@ -28,6 +28,8 @@ import threading
 import dash_bootstrap_components as dbc
 import datetime
 
+dropdown_options = [{'label': 'Aktiv', 'value': 'Aktiv'}, {'label': 'Inaktiv', 'value': 'Inaktiv'}]
+
 # Connection string for SQL Server
 cnxn_string = (
     'Driver={ODBC Driver 18 for SQL Server};'
@@ -79,7 +81,7 @@ def get_factor_names():
 
     # Query to fetch column names from the table
     # query = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'company_data'"
-    query = "SELECT * FROM factors"
+    query = "SELECT * FROM factors WHERE Status = 1"
 
     # Execute the query
     cursor.execute(query)
@@ -146,7 +148,7 @@ def get_unique_dates():
 cache = diskcache.Cache("./cache")
 long_callback_manager = DiskcacheLongCallbackManager(cache)
 
-app = dash.Dash(__name__, external_stylesheets=['styles.css',dbc.themes.BOOTSTRAP], long_callback_manager=long_callback_manager)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP,'styles.css'], long_callback_manager=long_callback_manager)
 output_file = "output.txt"
 open(output_file, 'w').close()
 
@@ -175,6 +177,60 @@ def restrict_access():
 
 column_options = [{'label': col, 'value': col} for col in get_column_names()]
 factor_options = [{'label': col, 'value': col} for col in get_factor_names()]
+
+# def initial_data_load_factors():
+#     with pyodbc.connect(cnxn_string) as conn:
+#         query = f"SELECT * FROM factors"
+#         print(query)
+#         df = pd.read_sql_query(query, conn)
+#         df['Status'] = df['Status'].apply(lambda x: 'Aktiv' if x == 1 else 'Inaktiv')
+#         print(df.head(5))
+#         dropdown_options = [{'label': 'Aktiv', 'value': 'Aktiv'}, {'label': 'Inaktiv', 'value': 'Inaktiv'}]
+#         # print(dropdown_options)
+#         # Update each row's dropdown options for the Status column
+#         # for index, row in df.iterrows():
+#         #     df.at[index, 'Status'] = {'options': dropdown_options, 'value': row['Status']}
+#
+#             # return df.to_dict('records'), {'Status': {'options': dropdown_options}}, ""
+#         # df = pd.read_excel(f'data_iter2/{file_name}')
+#
+#         return dash_table.DataTable(
+#             id='tanks-table',
+#             data=df.to_dict('records'),
+#             columns=[
+#                 {'name': i, 'id': i, 'presentation': 'dropdown' if i == 'Status' else 'input'}
+#                 for i in df.columns
+#             ],
+#             style_table={'overflowX': 'auto', 'width': '100%'},
+#             editable=True,  # Make the table editable
+#             dropdown={
+#                 'Status': {
+#                     'options': [
+#                         {'label': 'Aktiv', 'value': 'Aktiv'},
+#                         {'label': 'Inaktiv', 'value': 'Inaktiv'}
+#                     ]
+#                 }
+#             },
+#             style_data_conditional=[
+#                 {
+#                     'if': {
+#                         'filter_query': '{On/Off} = "On"',
+#                         'column_id': 'On/Off'
+#                     },
+#                     'backgroundColor': 'lightgreen',
+#                     'color': 'black'
+#                 },
+#                 {
+#                     'if': {
+#                         'filter_query': '{On/Off} = "Off"',
+#                         'column_id': 'On/Off'
+#                     },
+#                     'backgroundColor': 'tomato',
+#                     'color': 'white'
+#                 }
+#             ]
+#         )
+#     return html.Div(f"{file_name} data not available.")
 
 app.title = "Nachhaltigkeitsserver"
 # Define the layout of the app
@@ -254,7 +310,7 @@ app.layout = html.Div([
                 ]),
                 dcc.Dropdown(
                         id='column-select-dropdown',
-                        options=column_options,
+                        options=factor_options,
                         multi=True,
                         placeholder='Spalten filtern'
                     ),
@@ -345,12 +401,15 @@ app.layout = html.Div([
                             id="loading-factor-table",
                             type="default",  # You can choose from 'graph', 'cube', 'circle', 'dot', or 'default'
                             children=[
+                # html.Div(id='factors-container', children=initial_data_load_factors()),
                 dash_table.DataTable(
                     id='factor-table',
+                    css=[{"selector":".dropdown", "rule": "position: static",}],
                     columns=[
                         {'name': 'Name', 'id': 'Name'},
                         {'name': 'Mapping', 'id': 'Mapping'},
-                        {'name': 'Status', 'id': 'Status', 'presentation': 'dropdown'},
+                        {'name': 'Status', 'id': 'Status', 'editable': True,
+                         'presentation': 'dropdown'},
                         # Add more columns as needed
                     ],
                     data=[],
@@ -363,7 +422,7 @@ app.layout = html.Div([
                             ]
                         }
                     },
-                    style_table={'maxHeight': '80vh', 'overflowY': 'auto'},
+                    # style_table={'maxHeight': '80vh', 'overflowY': 'auto'},
                     style_cell_conditional=[
                             {'if': {'column_id': 'Status'}, 'width': '200px'},  # Set specific width for the ISIN column
                             # {'if': {'column_id': 'ISSUER_NAME'}, 'width': '200px'},  # Set specific width for the Issuer Name column
@@ -384,7 +443,7 @@ app.layout = html.Div([
                                 'column_id': 'Status'
                             },
                             'backgroundColor': 'tomato',
-                            'color': 'white'
+                            # 'color': 'white'
                         }
                     ]
                 ),
@@ -476,7 +535,8 @@ def display_filtered_company_data(isin_input):
 
 @app.callback(
     [Output('factor-table', 'data'),
-     Output('factor-table', 'dropdown'), Output('add-factor-output', 'children')],
+     # Output('factor-table', 'dropdown')],
+     Output('add-factor-output', 'children')],
     [
      Input('factor-table', 'data_previous'), Input('factor-select-dropdown', 'value'),Input('add-factor-button', 'n_clicks') ],
     [State('new-factor-name-input', 'value'), State('factor-table', 'data')]
@@ -487,54 +547,103 @@ def update_table( previous_data, factor_input_dropdown, n_clicks, new_factor_nam
 
     print('triggered')
     print(triggered_component)
-    # if triggered_component == 'dummy-div.children' or triggered_component == 'add-factor-button.n_clicks':
-    #     try:
-    #         with pyodbc.connect(cnxn_string) as conn:
-    #             # Fetch and display the existing data fields from the 'company_data' table
-    #             # query = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'company_data'"
-    #             query = "SELECT * FROM factors"
-    #             # existing_columns = [row.COLUMN_NAME for row in conn.execute(query)]
-    #
-    #         # data = [{'column_name': column_name} for column_name in existing_columns]
-    #     except Exception as e:
-    #         print(str(e))
-    # if triggered_component == 'add-factor-button':
-    #     print('buttonadd')
-    #     if not new_factor_name:
-    #         raise ValueError("Bitte geben einen Faktornamen ein")
-    #
-    #     with pyodbc.connect(cnxn_string) as conn:
-    #         print('buttonadd1')
-    #                 # Use the specified new_column_name and add it with the type nvarchar(50)
-    #         query = f"ALTER TABLE company_data ADD [{new_factor_name}] nvarchar(50)"
-    #         cursor = conn.cursor()
-    #         cursor.execute(query)
-    #         conn.commit()
-    #         print('buttonadd2')
-    #         query = f"INSERT INTO factors (Name, Status, Mapping) VALUES ('{new_factor_name}', 1, '{new_factor_name}')"
-    #
-    #         cursor = conn.cursor()
-    #         cursor.execute(query)
-    #         conn.commit()
+    if triggered_component == 'dummy-div.children' or triggered_component == 'add-factor-button.n_clicks':
+        try:
+            with pyodbc.connect(cnxn_string) as conn:
+                # Fetch and display the existing data fields from the 'company_data' table
+                # query = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'company_data'"
+                query = "SELECT * FROM factors ORDER BY ID DESC"
+                # existing_columns = [row.COLUMN_NAME for row in conn.execute(query)]
+
+            # data = [{'column_name': column_name} for column_name in existing_columns]
+        except Exception as e:
+            print(str(e))
+    if triggered_component == 'add-factor-button':
+        print('buttonadd')
+        if not new_factor_name:
+            error_message = "Bitte geben Sie einen Faktornamen ein."
+        else:
+            try:
+                with pyodbc.connect(cnxn_string) as conn:
+                    cursor = conn.cursor()
+                    # Add new factor to company_data table
+                    alter_query = f"ALTER TABLE company_data ADD [{new_factor_name}] nvarchar(50)"
+                    cursor.execute(alter_query)
+                    # Insert new factor into factors table
+                    insert_query = f"INSERT INTO factors (Name, Status, Mapping) VALUES ('{new_factor_name}', 1, '{new_factor_name}')"
+                    cursor.execute(insert_query)
+                    conn.commit()
+            except Exception as e:
+                error_message = f"An error occurred: {str(e)}"
 
     if not factor_input_dropdown:
         factor_input_dropdown = get_factor_names()  # Default to all columns if none are selected
 
     if triggered_component == 'factor-table':
+        if previous_data is not None:
+            # Find changes between current_data and previous_data
+            for i, (current, previous) in enumerate(zip(data_current, previous_data)):
+                if current != previous:
+                    # Assuming 'ID' is a unique identifier for each row in your SQL table
+                    factor_id = current['Id']
+                    # Generate SQL query to update changed values
+                    update_queries = []
+                    column_name_change_query = None
+                    old_name = None
 
-        return previous_data, dash.no_update, ""
+                    for key, value in current.items():
+                        if current[key] != previous[key]:
+                            if key == 'Status':
+                                if value == 'Aktiv':
+                                    update_query = f"UPDATE factors SET {key} = 1 WHERE Id = {factor_id}"
+                                    update_queries.append(update_query)
+                                else:
+                                    update_query = f"UPDATE factors SET {key} = 0 WHERE Id = {factor_id}"
+                                    update_queries.append(update_query)
+                            elif key == 'Name':
+                                old_name = previous[key]
+                                new_name = value
+                                update_query = f"UPDATE factors SET {key} = '{value}' WHERE Id = {factor_id}"
+                                update_queries.append(update_query)
+                                column_name_change_query = f"EXEC sp_rename 'company_data.[{old_name}]', '{new_name}', 'COLUMN';"
+                                update_queries.append(column_name_change_query)
+                            else:
+                                update_query = f"UPDATE factors SET {key} = '{value}' WHERE Id = {factor_id}"
+                                update_queries.append(update_query)
+                            print(update_query)
+
+
+
+                    # Execute SQL update queries
+                    try:
+                        with pyodbc.connect(cnxn_string) as conn:
+                            cursor = conn.cursor()
+                            for query in update_queries:
+                                cursor.execute(query)
+                            conn.commit()
+                    except Exception as e:
+                        error_message = f"An error occurred while updating data: {str(e)}"
+
+        # return previous_data, dash.no_update, ""
+        return data_current, ""
     else:
         conditions = " OR ".join([f"Mapping LIKE '{factor}'" for factor in factor_input_dropdown])
         print(conditions)
-        query = f"SELECT * FROM factors WHERE {conditions}"
+        query = f"SELECT * FROM factors WHERE {conditions} ORDER BY ID DESC "
         print(query)
         with pyodbc.connect(cnxn_string) as conn:
             df = pd.read_sql_query(query, conn)
             df['Status'] = df['Status'].apply(lambda x: 'Aktiv' if x == 1 else 'Inaktiv')
             print(df.head(5))
             dropdown_options = [{'label': 'Aktiv', 'value': 'Aktiv'}, {'label': 'Inaktiv', 'value': 'Inaktiv'}]
-            print(dropdown_options)
-            return df.to_dict('records'), {'Status': {'options': dropdown_options}}, ""
+            # print(dropdown_options)
+
+            # Update each row's dropdown options for the Status column
+            # for index, row in df.iterrows():
+            #     df.at[index, 'Status'] = {'options': dropdown_options, 'value': row['Status']}
+
+            # return df.to_dict('records'), {'Status': {'options': dropdown_options}}, ""
+            return df.to_dict('records'), ""
 
     # try:
     #     with pyodbc.connect(cnxn_string) as conn:
@@ -576,9 +685,29 @@ def update_data_table(selected_date, selected_isins, selected_columns):
         # Establish a connection to the SQL Server database
         conn = pyodbc.connect(cnxn_string)
 
+        # Fetch the mapping from the factors table where Status is 1 (enabled)
+        mapping_query = "SELECT Name, Mapping FROM factors WHERE Status = 1"
+        mapping_df = pd.read_sql_query(mapping_query, conn)
+        column_mapping = dict(zip(mapping_df['Name'], mapping_df['Mapping']))
+        reverse_mapping = {v: k for k, v in column_mapping.items()}
+        print('mapping df')
+        print(mapping_df)
         # Update the columns of the data table based on selected_columns
         if not selected_columns:
-            selected_columns = get_column_names()  # Default to all columns if none are selected
+            # selected_columns = get_column_names()  # Default to all columns if none are selected
+            selected_columns = mapping_df['Mapping'].tolist()
+            print(selected_columns)
+            print('hhhhh')
+        else:
+            # Ensure only enabled and selected columns are included
+            # reverse_mapping = {v: k for k, v in column_mapping.items()}
+            # selected_columns = [reverse_mapping.get(col, col) for col in selected_columns]
+            print(selected_columns)
+            print(mapping_df['Mapping'].tolist())
+            selected_columns = [col for col in selected_columns if col in mapping_df['Mapping'].tolist()]
+            print('ggggg')
+            print(selected_columns)
+
 
         # Build the SQL query based on selected_date and isins
         query = f"SELECT TOP 1000 * FROM company RIGHT JOIN company_data ON company.CompanyID = company_data.CompanyID WHERE DataDate = '{selected_date}'"
@@ -588,13 +717,14 @@ def update_data_table(selected_date, selected_isins, selected_columns):
 
         # Fetch and filter data from the database
         df = pd.read_sql_query(query, conn)
+        df.rename(columns=column_mapping, inplace=True)
 
         # Close the database connection
         conn.close()
         issuer_isin_column = {'name': 'ISSUER_ISIN', 'id': 'ISSUER_ISIN'}
         issuer_name_column = {'name': 'ISSUER_NAME', 'id': 'ISSUER_NAME'}
         # issuer_id_column = {'name': 'ISSUERID', 'id': 'ISSUERID'}
-        columns = [{'name': col, 'id': col} for col in selected_columns]
+        columns = [{'name': col, 'id': col} for col in df.columns if col in selected_columns]
         columns.insert(0, issuer_name_column)
         columns.insert(0, issuer_isin_column)
         # columns.insert(0, issuer_id_column)
